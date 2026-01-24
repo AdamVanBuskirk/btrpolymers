97c57988385b157c6edbbfb6223200e0cf1c203f
Adam VanBuskirk <adamvanbuskirk@Adams-MBP-3.lan>
2026-01-24 09:13:12 -0500

    tweak

--- client/src/Pages/Public/Home.tsx
+++ client/src/Pages/Public/Home.tsx
@@ -73,7 +73,7 @@ function Home() {
         <div className="main-container" style={{ background: "#fff" }}>
  
             <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-                <div>
+                <div style={{ maxWidth: isMobile ? "100%" : "80%" }}>
                     <h1 style={heading}>
                         Turning Industrial Plastic Waste into High-Performance Materials
                     </h1>
